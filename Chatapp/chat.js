11a29c612f917a6e0086efd06af547c9f9f66372
Thomas Kainz <kaithd17@htl-kaindorf.at>
2021-11-30 13:21:19 +0100

    done with chatapp

--- Chatapp/chat.js
+++ Chatapp/chat.js
@@ -1,1 +1,90 @@
-let ws = new WebSocket("ws://127.0.0.1:5678/")
+const ws = new WebSocket("ws://127.0.0.1:5678/");
+
+const getUsername = () => {
+  return document.getElementById("displayUsername").innerText.split(": ")[1];
+};
+
+const setUserCount = () => {
+  document.getElementById("onlineStatus").innerHTML =
+    "User" + (data.count == 1 ? "" : "s") + " Online: " + data.count;
+};
+
+const setValidUsername = () => {
+  let username = document.getElementById("username").value;
+  if (username !== "") {
+    document.getElementById("displayUsername").innerHTML =
+      "Username: " + username;
+    document.getElementById("username").style.display = "none";
+    document.getElementById("userButton").style.display = "none";
+    return true;
+  }
+  return false;
+};
+
+const registerUser = () => {
+  if (setValidUsername()) {
+    ws.send(JSON.stringify({ action: "newUser", username: getUsername() }));
+    document.getElementById("sendButton").disabled = false;
+  }
+};
+
+const showMessage = (data) => {
+  if (data.message != undefined && data.username != undefined) {
+    let username = getUsername();
+    let template;
+    let node;
+    if (username === data.username) {
+      template = document.getElementById("messageTemplate");
+      node = template.content.cloneNode(true);
+      node.getElementById("user").innerHTML = "You: ";
+    } else {
+      template = document.getElementById("messageTemplate2");
+      node = template.content.cloneNode(true);
+      node.getElementById("user").innerHTML = data.username + ": ";
+    }
+    node.getElementById("messageText").innerHTML = data.message + "";
+    document.getElementById("messages").appendChild(node);
+  }
+};
+
+const showInfoMessage = (data, action) => {
+  let template = document.getElementById("infoTemplate");
+  let node = template.content.cloneNode(true);
+  if (action === "addUser") {
+    node.getElementById("messageContainer2").style.backgroundColor = "#00b8f5";
+    node.getElementById("messageText").innerHTML =
+      data.username + " joined the chat.";
+  } else {
+    node.getElementById("messageContainer2").style.backgroundColor = "#d33030";
+    node.getElementById("messageText").innerHTML =
+      data.username + " left the chat.";
+  }
+  document.getElementById("messages").appendChild(node);
+};
+
+const sendMessage = () => {
+  document.getElementById("message").innerHTML = "";
+  let username = getUsername();
+  let message = document.getElementById("message").value;
+  if (message !== "") {
+    ws.send(
+      JSON.stringify({
+        action: "newMessage",
+        username: username,
+        message: message,
+      })
+    );
+    document.getElementById("message").value = "";
+  }
+};
+
+ws.onmessage = (e) => {
+  data = JSON.parse(e.data);
+  switch (data.type) {
+    case "users":
+      setUserCount();
+      showInfoMessage(data, data.action);
+    case "message":
+      showMessage(data);
+  }
+};
--- Chatapp/server.py
+++ Chatapp/server.py
@@ -1,11 +1,54 @@
 import asyncio
 import websockets
+import json
 
-async def test():
-    print("test")
+USERS = set()
+
+#This function builds a message with the current amount of users and if a user (=username) joined or left the chat (=action)
+def users_event(action, username):
+    return json.dumps({
+        "type":"users",
+        "username":username,
+        "count":len(USERS),
+        "action":action
+    })
+
+#This function adds a user and it sends a message with this information to all users
+def add_user(websocket, username):
+    USERS.add(websocket)
+    websockets.broadcast(USERS, users_event("addUser", username))
+
+#This function removes a user and it sends a message with this information to all users
+def remove_user(websocket, username):
+    USERS.remove(websocket)
+    websockets.broadcast(USERS, users_event("removeUser", username))
+
+#This function builds a message with the user who sent a message and it includes the message itself
+def message_event(data):
+    return json.dumps({
+        "type":"message",
+        "username":data["username"],
+        "message":data["message"]
+    })
+
+#This function sends the message to all users
+def send_message(data):
+    websockets.broadcast(USERS, message_event(data))
+
+#This function handles all requests which are coming from the users
+async def message_handler(websocket, path):
+    try:
+        async for message in websocket:
+            data = json.loads(message)
+            if data["action"] == "newUser":
+                add_user(websocket, data["username"])
+            elif data["action"] == "newMessage":
+                send_message(data)
+    finally:
+        remove_user(websocket, data["username"])
 
 async def main():
-    async with websockets.serve(test, "localhost", 5678):
+    async with websockets.serve(message_handler, "localhost", 5678):
         await asyncio.Future()
 
 asyncio.run(main())
